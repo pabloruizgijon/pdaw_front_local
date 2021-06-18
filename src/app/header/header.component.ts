56353b75596ef8cddf069fd7d24a703dc0cfb4b6
Pablo Lopez Burgos <pablo.lopez.external@atos.net>
2021-06-18 21:56:15 +0200

    update docker

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,7 +5,8 @@ import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
   title: string = 'App Angular'
